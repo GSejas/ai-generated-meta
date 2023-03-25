9c818cdc207219d174fa92a068a5fab92c351796
jorge <jorge.sequeirarojas@team.neustar>
2023-03-25 14:02:49 -0600

    + commiting json template queries + modifs in readme + refactored code to allow passing json query template as arg + added support to store project structure cached serialized

--- ai_header_generator/misc.py
+++ ai_header_generator/misc.py
@@ -19,6 +19,16 @@ def read_excludes(topdir, config):
     return (exclude_files, exclude_dirs)
 
 def generate_tree(topdir, config):
+    
+    tree_file = os.path.join(topdir, ".project_tree.hex")
+    
+    if os.path.exists(tree_file):
+        with open(tree_file, "rb") as f:
+            try:
+                tree = jsonpickle.decode(f.read())
+                return tree
+            except:
+                os.remove(tree_file)
 
     def walkdir(folder, d):
         
@@ -95,67 +105,9 @@ def generate_tree(topdir, config):
 
     tree = defaultdict(dict)
     walkdir(topdir, tree)
+    
+    # write the serialized data to file
+    with open("project_tree.hex", "wb") as f:
+        f.write(jsonpickle.encode(tree).encode())
+        
     return tree
-    # exclude_dirs = config.get('project', 'exclude_dirs', fallback='').split(', ')
-    # exclude_files = config.get('project', 'exclude_files', fallback='').split(', ')
-
-    # # Read ignore patterns from .gitignore file if it exists
-    # gitignore_path = os.path.join(topdir, '.gitignore')
-    # if os.path.exists(gitignore_path):
-    #     with open(gitignore_path, 'r') as f:
-    #         gitignore_patterns = f.read().splitlines()
-    #     exclude_files += gitignore_patterns
-
-    # def walkdir(folder, d):
-    #     if os.path.basename(folder) in exclude_dirs:
-    #         return
-    #     for name in os.listdir(folder):
-    #         try:
-    #             path = os.path.join(folder, name)
-    #             if os.path.islink(path):
-    #                 continue
-    #             if any(fnmatch.fnmatch(name, pattern) for pattern in exclude_files):
-    #                 continue
-    #             if any(fnmatch.fnmatch(path, pattern) for pattern in exclude_files):
-    #                 continue
-    #             if os.path.isdir(path):
-    #                 d[name] = {}
-    #                 walkdir(path, d[name])
-    #             else:
-    #                 d[name] = None
-    #             for name in os.listdir(folder):
-    #                 path = os.path.join(folder, name)
-    #                 if os.path.islink(path):
-    #                     continue
-    #                 if any(fnmatch.fnmatch(name, pattern) for pattern in exclude_files):
-    #                     continue
-    #                 if any(fnmatch.fnmatch(path, pattern) for pattern in exclude_files):
-    #                     continue
-    #                 if os.path.isdir(path):
-    #                     d[name] = {}
-    #                     walkdir(path, d[name])
-    #                 else:
-    #                     d[name] = None
-    #                 for name in os.listdir(folder):
-    #                     path = os.path.join(folder, name)
-    #                     if os.path.isdir(path):
-    #                         walkdir(path, d[name])
-    #                     else:
-    #                         d[name] = None
-    #         except Exception as e:
-    #             print(f"Error: {e}")
-    #             tb = traceback.extract_tb(e.__traceback__)
-    #             folders = set(os.path.dirname(frame.filename) for frame in tb)
-    #             files = set(frame.filename for frame in tb)
-    #             patterns = set()
-    #             for folder in folders:
-    #                 patterns.add(os.path.basename(folder))
-    #             for file in files:
-    #                 patterns.add(os.path.basename(file))
-    #             patterns.add(os.path.basename(path))
-    #             print(f"Suggested exclude patterns: {','.join(patterns)}")
-    #             continue
-
-    # tree = defaultdict(dict)
-    # walkdir(topdir, tree)
-    # return tree
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="ai_header_generator",
     version="0.1",
-    packages=find_packages(),
+    packages=find_packages(include=['ai_header_generator', 'ai_header_generator.*']),
     install_requires=[
         "openai",'argparse','configparser','jsonpickle'
     ],
@@ -12,4 +12,8 @@ setup(
             "ai-header-generator=cli:main",
         ],
     },
+    include_package_data=True,
+    package_data={
+        '': ['*.json'],
+    }
 )
